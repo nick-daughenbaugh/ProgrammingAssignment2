8b537afb46c13db26c328a0ddf4a097a1956a23e
nick-daughenbaugh <54877295+nick-daughenbaugh@users.noreply.github.com>
2019-09-27 07:09:22 -0700

    Add files via upload

---
+++ makeCacheMatrix.R
@@ -0,0 +1,28 @@
+makeCacheMatrix <- function(x = matrix()){ 
+  
+  I <- NULL 
+  
+  ## set the matrix x 
+  set <- function(m){ 
+    x <<- m 
+    I <<- NULL 
+  } 
+  
+  ## get the matrix x 
+  get <- function(){ 
+    x                
+  }  
+  
+  ## set the inverse matrix of x 
+  setInv <- function(i) { 
+    I <<- i 
+  } 
+  
+  ## get the inverse matrix of x 
+  getInv <- function() { 
+    I 
+  } 
+  
+  ## list of internal methods 
+  list(set = set, get = get, setInv = setInv, getInv = getInv) 
+} 
